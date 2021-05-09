9061b30b147e14c945d8e6a64d5dbe153f887f84
tszyborski <tszyborski@sii.pl>
2021-05-09 12:22:48 +0200

    Reactor example

--- src/test/java/com/testinglaboratory/restassured/reactor/ExampleReactorTest.java
+++ src/test/java/com/testinglaboratory/restassured/reactor/ExampleReactorTest.java
@@ -1,4 +1,23 @@
 package com.testinglaboratory.restassured.reactor;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.when;
+
 public class ExampleReactorTest {
+
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.baseURI = "http://localhost:8083/";
+        RestAssured.basePath = "challenge/reactor/";
+    }
+
+    @Test
+    void checkInformation(){
+       when().get("information")
+       .then().log().everything();
+    }
 }
