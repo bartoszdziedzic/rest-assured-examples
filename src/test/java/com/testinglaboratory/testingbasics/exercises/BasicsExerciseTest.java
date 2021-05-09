b82fc36fa5add059862382a8f3228b714b2b14a6
bdziedzic <bdziedzic@sii.pl>
2021-05-09 11:35:48 +0200

    basicToyExcercise

--- src/test/java/com/testinglaboratory/testingbasics/exercises/BasicsExerciseTest.java
+++ src/test/java/com/testinglaboratory/testingbasics/exercises/BasicsExerciseTest.java
@@ -1,5 +1,11 @@
 package com.testinglaboratory.testingbasics.exercises;
 
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /*TODO EXERCISE create tests checking:
    - number of letters in your first name
    - equality of length of your first and last name
@@ -7,4 +13,28 @@ package com.testinglaboratory.testingbasics.exercises;
    - your first name having more than 3 letters
  */
 public class BasicsExerciseTest {
+    String firstName = "Bartosz";
+    String lastName = "Dziedzic";
+
+
+
+    @Test
+    public void lettersInName(){
+        assertThat(firstName.length()).as("Wyjebało się.").isEqualTo(7);
+    }
+
+    @Test
+    public void lettersInFirstAndLast(){
+        assertThat(firstName.length()).isEqualTo(lastName.length());
+    }
+
+    @Test
+    public void firstAndLastNamesEqual(){
+        assertEquals(firstName,lastName);
+    }
+
+    @Test
+    public void firstNameHasMoreThanThreeLetters(){
+        assertThat(firstName).hasSizeGreaterThan(3);
+    }
 }
--- src/test/java/com/testinglaboratory/testingbasics/exercises/WholesomeExerciseTest.java
+++ src/test/java/com/testinglaboratory/testingbasics/exercises/WholesomeExerciseTest.java
@@ -1,5 +1,9 @@
 package com.testinglaboratory.testingbasics.exercises;
 
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /*TODO EXERCISE
  * - Create class Toy having fields: make, name, colour, material
  *  - having methods returning greeting (including name, colour and material)
@@ -10,4 +14,45 @@ package com.testinglaboratory.testingbasics.exercises;
  *  - remember to clean up after tests
  */
 public class WholesomeExerciseTest {
+
+    @BeforeEach
+    public void createFile(){
+        FileManager.createFile("MyTestFolder/toy.txt");
+    }
+
+    @BeforeAll
+    public static void createFolder(){
+        FileManager.createDirectory("MyTestFolder");
+    }
+
+    @AfterEach
+    public void deleteCreatedFile(){
+        FileManager.deleteFile("MyTestFolder/toy.txt");
+    }
+
+    @AfterAll
+    public static void deleteCreatedFolder(){
+        FileManager.deleteFile("MyTestFolder");
+    }
+
+    @Test
+    public void basicPrintOutTest(){
+        Toy toy = new Toy("Lego","Star Wars","Yoda","Green");
+        System.out.println(toy.greeting());
+    }
+
+    @Test
+    public void writeToFile(){
+        Toy toy = new Toy("Lego","Star Wars","Yoda","Green");
+        FileManager.writeToFileFile("MyTestFolder/toy.txt", toy.toString());
+    }
+
+    @Test
+    public void readFile(){
+        Toy toy = new Toy("Lego","Star Wars","Yoda","Green");
+        FileManager.writeToFileFile("MyTestFolder/toy.txt", toy.toString());
+        String toyText = FileManager.readFile("MyTestFolder/toy.txt");
+        assertThat(toyText).isEqualTo(toy.toString());
+
+    }
 }
